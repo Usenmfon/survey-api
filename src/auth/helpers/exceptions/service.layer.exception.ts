d2092eb73483f9a1d5349e21861bfcf3685c496c
Usenmfon Uko <usenmfonuko@gmail.com>
2023-03-05 13:10:52 +0100

    commented public error status code

--- src/auth/helpers/exceptions/service.layer.exception.ts
+++ src/auth/helpers/exceptions/service.layer.exception.ts
@@ -1,7 +1,7 @@
 import { IErrorResponse } from 'src/auth/interfaces/response.interface';
 
 export class ServiceException extends Error {
-  public code = 400;
+  //public code = 400;
   constructor(error: IErrorResponse) {
     super(error.error);
     this.name = 'Service Exception';
